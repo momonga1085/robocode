86c9f6ed54e31e9d4b788d16ed2f6f6f92438ba2
momonga1085 <momonga1085@gmail.com>
2019-08-05 08:06:49 +0900

    Add files via upload

--- components/App.vue
+++ components/App.vue
@@ -1,106 +1,236 @@
 <template>
-<div>
-<head>
-  <link href='https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900|Material+Icons' rel="stylesheet">
-</head>
-  <v-app>
-    <!-- <side-menu></side-menu>
+  <div>
+
+    <head>
+      <link
+        href='https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900|Material+Icons'
+        rel="stylesheet"
+      >
+    </head>
+    <v-app>
+      <!-- <side-menu></side-menu>
     <table2></table2> -->
-    <!-- <diagram :chartData="chartData"></diagram> -->
-    
-     <!-- <chart></chart> -->
-     <diagram-chart></diagram-chart>
-     <kpi-chart></kpi-chart>
-  </v-app>
+      <!-- <diagram :chartData="chartData"></diagram> -->
+      <BarChart :chartData="chartData"></BarChart>
+      <!-- <Heatmap :chartData="chartData"></Heatmap> -->
+      <!-- <chart></chart> -->
+      <!-- <diagram-chart></diagram-chart> -->
+      <!-- <kpi-chart></kpi-chart> -->
+    </v-app>
   </div>
 </template>
 
 <script>
-import SideMenu from './components/SideMenu'
+import SideMenu from "./components/SideMenu";
 // import d3chart from './components/d3chart'
-import table from './components/table'
-import diagram from './components/Diagram'
-import DiagramChart from './components/DiagramChart'
-import KpiChart from './components/KpiChart'
+import table from "./components/table";
+import diagram from "./components/Diagram";
+import diagram2 from "./components/d3/DiagramChart";
+import DiagramChart from "./components/DiagramChart";
+import KpiChart from "./components/KpiChart";
+import Heatmap from "./components/Heatmap";
+import BarChart from "./components/BarChart";
+
+// var chartData = [
+//   {
+//     serialNo: "aaa",
+//     values: [
+//       {
+//         date: "2007-10-15 08:15",
+//         value: "工程1",
+//         z: 10,
+//         message: "aaaa"
+//       },
+//       {
+//         date: "2007-10-15 08:30",
+//         value: "工程2",
+//         z: 60,
+//       },
+//       {
+//         date: "2007-10-15 08:00",
+//         value: "工程3",
+//         z: 30,
+//       },
+//       {
+//         date: "2007-10-15 08:45",
+//         value: "工程4",
+//         z: 100,
+//       },
+//       {
+//         date: "2007-10-15 09:00",
+//         value: "工程5",
+//         z: 20,
+//       }
+//     ]
+//   },
+// ];
+
+// var chartData = [
+//   {
+//     serialNo: "aaa",
+//     values: [
+//       {
+//         date: "2007-09-28",
+//         value: "工程1",
+//         z: 10,
+//         message: "aaaa"
+//       },
+//       {
+//         date: "2007-10-02",
+//         value: "工程2",
+//         z: 60,
+//       },
+//       {
+//         date: "2007-10-03",
+//         value: "工程3",
+//         z: 30,
+//       },
+//       {
+//         date: "2007-10-04",
+//         value: "工程4",
+//         z: 100,
+//       },
+//       {
+//         date: "2008-01-01",
+//         value: "工程5",
+//         z: 20,
+//       }
+//     ]
+//   },
+//   {
+//     serialNo: "bbb",
+//     values: [
+//       {
+//         date: "2007-09-27",
+//         value: "工程1",
+//         message: "aaaa",
+//         z: 60,
+//       },
+//       {
+//         date: "2007-09-30",
+//         value: "工程1",
+//         message: "aaaa",
+//         z: 60,
+//       },
+//       {
+//         date: "2007-10-01",
+//         value: "工程2",
+//         z: 60,
+//       },
+//       {
+//         date: "2007-10-02",
+//         value: "工程3",
+//         z: 60,
+//       },
+//       {
+//         date: "2007-10-03",
+//         value: "工程4",
+//         z: 60,
+//       },
+//       {
+//         date: "2007-10-04",
+//         value: "工程5",
+//         z: 60,
+//       },
+//       {
+//         date: "2007-10-31",
+//         value: "工程6",
+//         z: 60,
+//       }
+//     ]
+//   }
+// ];
 
 var chartData = [
-  [
-    {
-      date: "2007-09-28",
-      value: 1
-    },
-    {
-      date: "2007-10-02",
-      value: 2
-    },
-    {
-      date: "2007-10-03",
-      value: 3
-    },
-    {
-      date: "2007-10-04",
-      value: 4
-    },
-    // {
-    //   date: "2008-01-01",
-    //   value: 33
-    // },
-  ],
-  [
-    {
-      date: "2007-02-01",
-      value: 10
-    },
-    {
-      date: "2007-10-01",
-      value: 30
-    },
-    {
-      date: "2007-10-02",
-      value: 55
-    },
-    {
-      date: "2007-10-08",
-      value: 50
-    },
-  ]
-];
+  {
+    serialNo: "aaa",
+    values: [
+      {
+        date: "1",
+        value: "工程1",
+        z: 10,
+        message: "aaaa"
+      },
+      {
+        date: "2",
+        value: "工程3",
+        z: 10,
+        message: "aaaa"
+      },
+    ]
+  },
+  {
+    serialNo: "aaa",
+    values: [
+      {
+        date: "1",
+        value: "工程2",
+        z: 10,
+        message: "aaaa"
+      },
+      {
+        date: "2",
+        value: "工程2",
+        z: 10,
+        message: "aaaa"
+      },
+    ]
+  },
+  {
+    serialNo: "aaa",
+    values: [
+      {
+        date: "1",
+        value: "工程2",
+        z: 10,
+        message: "aaaa"
+      },
+      {
+        date: "2",
+        value: "工程2",
+        z: 10,
+        message: "aaaa"
+      },
+    ]
+  }
+]
 
 export default {
-  name: 'app',
-  data: function () {
+  name: "app",
+  data: function() {
     return {
-      lines : [[1,2],[10,23],[222,323]],
-      chartData: chartData,
-    }
+      lines: [[1, 2], [10, 23], [222, 323]],
+      chartData: chartData
+    };
   },
-  methods:{
-    select (val) {
-      console.log(val)
+  methods: {
+    select(val) {
+      console.log(val);
     }
   },
   components: {
-    "side-menu" : SideMenu,
+    "side-menu": SideMenu,
     // "chart" : d3chart,
-    "table2": table,
-    "diagram" : diagram,
-    "diagram-chart" : DiagramChart,
-    "kpi-chart":KpiChart
-  },created () {
-      var lines = [];
-        for (var i = 0; i < 2; i++) {
-          var line = [];
-          for (var j = 0; j < 11; j++) {
-            line.push([j, Math.floor(Math.random() * 10)
-            ]);
-          }
-          lines.push(line);
-        }
-        this.lines = lines;
-        console.log(lines)
+    table2: table,
+    diagram: diagram,
+    "diagram-chart": DiagramChart,
+    "kpi-chart": KpiChart,
+    Heatmap : Heatmap,
+    BarChart: BarChart
+  },
+  created() {
+    var lines = [];
+    for (var i = 0; i < 2; i++) {
+      var line = [];
+      for (var j = 0; j < 11; j++) {
+        line.push([j, Math.floor(Math.random() * 10)]);
+      }
+      lines.push(line);
     }
-  
-}
+    this.lines = lines;
+    console.log(lines);
+  }
+};
 </script>
 
 <style>
